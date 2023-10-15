441dc6a06eb1152c8f41b2c8f9042195105caba3
leviathan <jake66martin@hotmail.com>
2023-10-15 12:06:43 -0400

    added conditions to input, trying to work on logo generation

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
+const logo = require("./Utils/generateLogo.js")
+const hexPattern = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
+
+
+function writeToFile(response) {
+  
+    fs.writeFile(`${response.shape}.svg`, logo.generateLogo(response), (err) =>
+    err ? console.error(err) : console.log('Mission success human!')
+    );
+    
+    console.log(response);
+    }
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What characters will your logo display? (up to 3 characters max)',
+      name: 'text',
+      validate: (input) => {
+        if (input.length > 3) {
+          return 'Length must be 3 characters or less.';
+        }
+        return true;
+      },
+    },
+    {
+      type: 'input',
+      message: 'Enter a hexidecimal in order to choose the text colour',
+      name: 'textColours',
+      validate: (input) => {
+        if (hexPattern.test(input) === true) {
+          return true;
+        }
+        return "Must be a valid hexidecimal code."
+      },
+    },
+    {
+      type: 'list',
+      message: 'Please choose from one of the following shape options',
+      name: 'shape',
+      choices: ["Square", "Circle", "Triangle"]
+    },
+    {
+        type: 'input',
+        message: 'Enter a hexidecimal in order to choose the shape colour',
+        name: 'shapeColours',
+        validate: (input) => {
+            if (hexPattern.test(input) === true) {
+              return true;
+            }
+            return "Must be a valid hexidecimal code."
+          },
+      },
+  ])
+  .then((response) => {
+    console.log("Success on making your logo!");
+    logo.generateLogo(response);
+    writeToFile(response);
+  }
+  );
