414ef3d14f521bd35bec3b97d885a82ffba4b385
leviathan <jake66martin@hotmail.com>
2023-10-18 21:27:56 -0400

    Managed to get the code working with classes

--- index.js
+++ index.js
@@ -6,77 +6,84 @@ const triangle = require("./Lib/triangle.js")
 const hexPattern = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
 
 
-// function writeToFile(response) {
-  
-//     fs.writeFile(`${response.shape}.svg`, logo.generateLogo(response), (err) =>
-//     err ? console.error(err) : console.log('Mission success human!')
-//     );
-    
-//     console.log(response);
-//     }
+function writeToFile(response) {
 
-// function init () {
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       message: 'What characters will your logo display? (up to 3 characters max)',
-//       name: 'text',
-//       validate: (input) => {
-//         if (input.length > 3) {
-//           return 'Length must be 3 characters or less.';
-//         }
-//         return true;
-//       },
-//     },
-//     {
-//       type: 'input',
-//       message: 'Enter a hexidecimal in order to choose the text colour',
-//       name: 'textColours',
-//       validate: (input) => {
-//         if (hexPattern.test(input) === true) {
-//           return true;
-//         }
-//         return "Must be a valid hexidecimal code."
-//       },
-//     },
-//     {
-//       type: 'list',
-//       message: 'Please choose from one of the following shape options',
-//       name: 'shape',
-//       choices: ["Square", "Circle", "Triangle"]
-//     },
-//     {
-//         type: 'input',
-//         message: 'Enter a hexidecimal in order to choose the shape colour',
-//         name: 'shapeColours',
-//         validate: (input) => {
-//             if (hexPattern.test(input) === true) {
-//               return true;
-//             }
-//             return "Must be a valid hexidecimal code."
-//           },
-//       },
-//   ])
-//   .then((response) => {
-//     console.log("Success on making your logo!");
-//     logo.generateLogo(response);
-//     writeToFile(response);
-    
-    
-  
-    
-      
-//   }
-//   );
-// }
+  fs.writeFile(`${response.shape}.svg`, shapeData, (err) =>
+    err ? console.error(err) : console.log('Mission success human!')
+  );
+
+  console.log(response);
+}
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What characters will your logo display? (up to 3 characters max)',
+        name: 'text',
+        validate: (input) => {
+          if (input.length > 3) {
+            return 'Length must be 3 characters or less.';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        message: 'Enter a hexidecimal in order to choose the text colour',
+        name: 'textColours',
+        validate: (input) => {
+          if (hexPattern.test(input) === true) {
+            return true;
+          }
+          return "Must be a valid hexidecimal code."
+        },
+      },
+      {
+        type: 'list',
+        message: 'Please choose from one of the following shape options',
+        name: 'shape',
+        choices: ["Square", "Circle", "Triangle"]
+      },
+      {
+        type: 'input',
+        message: 'Enter a hexidecimal in order to choose the shape colour',
+        name: 'shapeColours',
+        validate: (input) => {
+          if (hexPattern.test(input) === true) {
+            return true;
+          }
+          return "Must be a valid hexidecimal code."
+        },
+      },
+    ])
+    .then((response) => {
 
 
 
-//   init();
-  
+      if (response.shape === "Circle") {
+        const circle1 = new circle.Circle(response.text, response.textColours, response.shapeColours)
+        shapeData = circle1.render()
+        writeToFile(response)
+      } else if (response.shape === "Square") {
+        const square1 = new square.Square(response.text, response.textColours, response.shapeColours)
+        writeToFile(response)
+      } else if (response.shape === "Triangle") {
+        const triangle1 = new triangle.Triangle(response.text, response.textColours, response.shapeColours)
+        shapeData = triangle1.render()
+        writeToFile(response)
+      }
 
-let yolo = new square.Square()
-console.log(yolo.render())
+    }
+    );
+}
+
+
+
+init();
+
+
+
 
 
